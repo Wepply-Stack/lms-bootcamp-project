5bf8906eab2a221cd5cb6d0a8b1ea669eddf1e6e
DammyDee <damola.adewunmi@gmail.com>
2026-04-06 09:14:38 +0100

    Setup a simple routing which will be worked on later to allow for routing to different path after authentication/authorisation are handled (note also a main layout has been created in layout directory and a home page has been created and added to pages directory)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import MainLayout from "./layout/MainLayout"
 
 
 function App() {
-  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
   return (
-    <>
-      
-    </>
+    <RouterProvider router={router} />
   )
 }
 
