6b6debfe1d9920b46215c636d7fb1ce7dbdb4a6e
its_jedu <olajideweb@gmail.com>
2026-04-17 15:26:03 +0100

    Chore: Backend implementation fully tested and functional

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# PostgreSQL  
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'webbly_db',
-        'USER': 'webbly_user',
-        'PASSWORD': 'Admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# PostgreSQL  
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Webbly_db',
+#         'USER': 'Webbly_user',
+#         'PASSWORD': 'Admin123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/settings/#auth-password-validators
